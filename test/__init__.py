0a9ec2f17290dd93e6ce9dfdbb3d46e0c8ba270d
bigboss3333 <steven.t.murphy3@gmail.com>
2023-10-11 12:05:01 -0400

    Some small test updates

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+import pathlib
+
+TEST_DIR = pathlib.Path(__file__).parent
+DATA = pathlib.Path(__file__).parent.parent / 'phylogenetics' / 'influenza-a'
--- test/test_make_phylogenetic_tree.py
+++ test/test_make_phylogenetic_tree.py
@@ -5,21 +5,27 @@ from unittest import TestCase
 
 from Bio import Phylo
 
-from phylogenetics.make_phylogenetic_tree import generate_tree
+from phylogenetics.make_phylogenetic_tree import generate_tree, graph_tree
+from test import TEST_DIR, DATA
 
 
 class TestTreeGen(TestCase):
     def test_generate_tree(self):
-        #with tempfile.TemporaryDirectory as tmp:
-        tmp = tempfile.mkdtemp()
-        base_path = pathlib.Path(tmp)
-        alignment_path = base_path / 'influenza-a.aln'
-        with open(alignment_path, 'w', encoding='utf-8') as aln_writer:
-            aln_writer.write('CLUSTAL'+os.linesep)
-            aln_writer.write('Influenza-A_1918      NGCCTGCTTTTGCTATT-------------------------------------------'+os.linesep)
-            aln_writer.write('Influenza-A_H1N1      AGCCTAATTATTGCTGCTAGGAACATAGTGAGAAGAGCTGCAGTGTCAGCAGATCCACTA'+os.linesep)
-            aln_writer.write('                       ****                                                       '+os.linesep)
+        with tempfile.TemporaryDirectory as tmp:
+            base_path = pathlib.Path(tmp)
+            alignment_path = base_path / 'influenza-a.aln'
+            with open(alignment_path, 'w', encoding='utf-8') as aln_writer:
+                aln_writer.write('CLUSTAL' + os.linesep)
+                aln_writer.write(
+                    'Influenza-A_1918      NGCCTGCTTTTGCTATT-------------------------------------------' + os.linesep)
+                aln_writer.write(
+                    'Influenza-A_H1N1      AGCCTAATTATTGCTGCTAGGAACATAGTGAGAAGAGCTGCAGTGTCAGCAGATCCACTA' + os.linesep)
+                aln_writer.write(
+                    '                       ****                                                       ' + os.linesep)
+            tree = generate_tree(alignment_path, True)
+            Phylo.draw_ascii(tree)
+            graph_tree(tree)
 
-        tree = generate_tree(alignment_path, True)
-        Phylo.draw_ascii(tree)
-
+    def test_vars(self):
+        self.assertIn('phylo_tree_generator/test', str(TEST_DIR))
+        self.assertIn('influenza-a', str(DATA))
